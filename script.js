b0b9235695f7b991aa00ff41ed00731f7cafde42
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-12 07:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-//your JS code here. If required.
+function clickHandler() {
+  let div = document.getElementById("output")
+  function promise1() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+		  //alert(document.getElementById("ip").value)
+        resolve(document.getElementById("ip").value)
+      }, 2000)
+    })
+  }
+  function promise2(num){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(num*2)
+      }, 2000)
+    })
+  }
+  function promise3(num){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(num-1)
+      }, 1000)
+    })
+  }
+  function promise4(num){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(num/2)
+      }, 1000)
+    })
+  }
+  function promise5(num){
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(num+10)
+      }, 1000)
+    })
+  }
+  promise1().then((res)=>{
+	  //alert(res)
+    div.textContent=`Result: ${res}`;
+    return promise2(res)
+  }).then((res)=>{
+    div.textContent=`Result: ${res}`;
+    return promise3(res)
+  }).then((res)=>{
+    div.textContent=`Result: ${res}`;
+    return promise4(res)
+  }).then((res)=>{
+    div.textContent=`Result: ${res}`;
+    return promise5(res)
+  }).then((res)=>{
+    div.textContent=`Result: ${res}`;
+    
+  })
+}
